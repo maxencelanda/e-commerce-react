4b8c39d2017d90a77b25634d34849d181d146f59
maxencelanda <moukasland@gmail.com>
2024-03-08 23:11:49 +0100

    filter (not functionnal)

--- src/components/Catalogue.jsx
+++ src/components/Catalogue.jsx
@@ -1,44 +1,58 @@
 import ProductCard from "./ProductCard";
 
 import getProducts from "../API";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Catalogue(){
 
     const [productArray, setProductArray] = useState([])
-    //const [searchInput, setSearchInput] = useState("")
     const [categories, setCategories] = useState([])
+    const [searchInput, setSearchInput] = useState("")
     //const [selectedCategories, setSelectedCategories] = useState([])
 
-    
-
-    getProducts().then((element) => {
-        let product = []
-        let categs = []
-        element.data.map((data, idx) => {
-            product.push(<ProductCard key={idx} d={data}/>)
-            if (!(categs.includes(data.category))){
-                categs.push(data.category)
-            }
+    const refreshProducts = () => {
+        getProducts().then((element) => {
+            let product = []
+            let categs = []
+            element.data.map((data, idx) => {
+                product.push(<ProductCard key={idx} d={data}/>)
+                if (!(categs.includes(data.category))){
+                    categs.push(data.category)
+                }
+            })
+            setProductArray(product)
+            setCategories(categs)
         })
-        setProductArray(product)
-        setCategories(categs)
-    })
+    }
+    
+    refreshProducts()
+
+    const searchProducts = (searchValue) => {
+        setSearchInput(searchValue)
+        if (searchInput != "") {
+            setProductArray(productArray.filter((item) =>item.props.d.title.substr(0, searchInput.length).toLowerCase() == searchInput))
+            console.log(productArray)
+        } else {
+            setProductArray([])
+            setCategories([])
+            refreshProducts()
+        }
+    }
 
     return (
         <div className="grid grid-cols-3">
             <div className="mx-auto border-r w-10/12">
                 <div>
-                    <label for="searchbox" className="font-bold mx-5">Search</label>
-                    <input name="searchbox" type="" className="border border-1 rounded-md pl-1"></input>
+                    <label htmlFor="searchbox" className="font-bold mx-5">Search</label>
+                    <input name="searchbox" type="" className="border border-1 rounded-md pl-1" onChange={e => searchProducts(e.target.value)}></input>
                 </div>
                 <div className="my-10">
                     <p className="font-bold text-center mb-5">Category</p>
                     {categories.map((category, idx) => 
                         <div key={idx}>
                             <input type="checkbox" name={"check"+idx} className="mx-2" checked></input>
-                            <label for={"check"+idx}>{category}</label>
+                            <label htmlFor={"check"+idx}>{category}</label>
                         </div>
                     )}
                 </div>
